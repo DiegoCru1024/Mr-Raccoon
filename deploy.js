cb1a765e98bc4c4d9d77ff08102d419d3b455388
MeekoDev <DiegoCru1024@hotmail.com>
2022-10-23 21:30:33 -0500

    Delete Files

--- deploy.js
+++
@@ -1,9 +0,0 @@
-function callCommands(){
-    client.application.commands.set([
-        {
-            name: "8ball",
-            description: "El señor mapache responde tus preguntas OwO",
-            options: []
-        }
-    ])
-}
